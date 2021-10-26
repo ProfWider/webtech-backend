3d0365aa4053e1d64c60ba7f258d068bcb9785ef
Prof. Dr. Arif Wider <wider@htw-berlin.de>
2021-10-27 00:15:42 +0200

    adds env readout

--- src/main/java/htw/berlin/m1/GreetingController.java
+++ src/main/java/htw/berlin/m1/GreetingController.java
@@ -1,33 +1,35 @@
 package htw.berlin.m1;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @Controller
 public class GreetingController {
 
-    @GetMapping("/greeting")
-    public String greeting(
-            @RequestParam(name="name", required=false, defaultValue="World") String name,
-            Model model) {
-        model.addAttribute("attr", name);
+    @Autowired
+    private Environment env;
+
+    @GetMapping("/")
+    public String greeting(Model model) {
+        model.addAttribute("attr", "World");
         return "greeting";
     }
 
     @GetMapping("/queryparam")
-    public String greeting(
-            @RequestParam(name="name", required=false, defaultValue="World") String name,
-            @RequestParam(name="par2", required=false) String par2,
+    public String queryparam(
+            @RequestParam(name="envar", required=false) String envar,
             Model model) {
-        model.addAttribute("attr", name+par2);
+        String enval = Optional.ofNullable(env.getProperty(envar)).orElse("Environment variable not found");
+        model.addAttribute("attr", enval);
         return "greeting";
     }
     @GetMapping("/path/param/{name}/{par2}")
-    public String greeting2(
+    public String pathparam(
             @PathVariable(name="name") String name,
             @PathVariable(name="par2") String par2,
             Model model) {
