59f5e5a6c51638e87ceb34427717b0bd20804a52
Prof. Dr. Arif Wider <wider@htw-berlin.de>
2021-11-08 22:56:54 +0100

    M1Application -> ExampleApplication

--- src/main/java/htw/berlin/ExampleApplication.java
+++ src/main/java/htw/berlin/ExampleApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class M1Application {
+public class ExampleApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(M1Application.class, args);
+		SpringApplication.run(ExampleApplication.class, args);
 	}
 
 }
--- src/test/java/htw/berlin/ExampleApplicationTests.java
+++ src/test/java/htw/berlin/ExampleApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class M1ApplicationTests {
+class ExampleApplicationTests {
 
 	@Test
 	void contextLoads() {
