3ac485df25f707f62178ffa6ca492aa1a8c3a06e
Prof. Dr. Arif Wider <wider@htw-berlin.de>
2022-01-05 01:59:32 +0100

    set active profiles per env

--- src/main/java/htw/berlin/SecurityConfig.java
+++ src/main/java/htw/berlin/SecurityConfig.java
@@ -1,12 +1,14 @@
 package htw.berlin;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @EnableWebSecurity
-@Profile(value = {"development", "production"})
+@Configuration
+@Profile(value = {"dev", "prod"})
 class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
@@ -14,5 +16,17 @@ class SecurityConfig extends WebSecurityConfigurerAdapter {
         http.cors().and().authorizeRequests()
                 .anyRequest().authenticated()
                 .and().oauth2ResourceServer().jwt();
+    }
+}
+
+@EnableWebSecurity
+@Configuration
+@Profile("test")
+class SecurityConfigTest extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.cors().and().authorizeRequests()
+                .anyRequest().authenticated();
     }
 }
